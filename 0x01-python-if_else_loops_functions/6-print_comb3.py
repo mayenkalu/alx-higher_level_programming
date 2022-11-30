43f523c2b19b109f28b6c1105d23cad2bce91eeb
Mayen Kalu <kalumayen@gmail.com>
2022-11-29 17:31:59 -0800

    fixed code

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if (i != j and i < j) and i < 9:
-            if (i == 8 and j == 9):
-                print('{0}{1}'.format(i, j))
-            else:
-                print('{0}{1}, '.format(i, j), end='')
+for i in range(0, 10):
+    for j in range(0, 10):
+        if (i < j):
+            print("{}{}".format(i, j), end="")
+            if (i < 8):
+                print(", ", end="")
+print("\n", end="")
