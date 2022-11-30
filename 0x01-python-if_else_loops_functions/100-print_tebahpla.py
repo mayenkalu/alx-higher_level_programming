5d9035eed92951d7117860f4053c96134521e9b4
Mayen Kalu <kalumayen@gmail.com>
2022-11-29 17:38:39 -0800

    fixed code

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in reversed(range(97, 123)):
-    if (i % 2 == 0):
-        print('{:c}'.format(i), end='')
-    else:
-        print('{:c}'.format(i - 32), end='')
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{:c}".format((c - (ord('a') - ord('A'))) if c % 2 else c), end='')
