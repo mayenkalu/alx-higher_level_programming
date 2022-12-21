27d7a9a2cd4ec964d7848fd659d59a712b162aac
Mayen Kalu <kalumayen@gmail.com>
2022-12-20 20:08:22 -0800

    class square with size instance

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,16 +10,17 @@ class Square:
     """Defines a square"""
 
     def __init__(self, size=0):
-        """Sets the size attribute of the square object,
+        """Sets the size attribute of the square object.
 
-        Checks that the size is an integer
+        Args:
+            size(int): the size of one side of the square.
 
+        Raises:
+            TypeError: if size is an integer.
+            ValueError: if size is grater than or equal to 0.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        """Checks that the size is greater than or equal to 0.
-
-        """
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
