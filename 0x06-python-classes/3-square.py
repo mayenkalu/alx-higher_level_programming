09968ced822288e71c4209b11e740bb13d45d0d6
Mayen Kalu <kalumayen@gmail.com>
2022-12-20 20:28:34 -0800

    on class square matter

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This is a square module.
+This module contains a class that defines a square
+initialise its size and verify that its size is an integer
+and it is greater than or equal to 0.
+"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        """Sets the size attribute of the square object.
+        Args:
+            size(int): the size of one side of the square.
+        Raises:
+            TypeError: if size is an integer.
+            ValueError: if size is grater than or equal to 0.
+        """
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""This is a square module.
+This module contains a class that defines a square
+initialise its size and verify that its size is an integer
+and it is greater than or equal to 0.
+"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        """Sets the size attribute of the square object.
+        Args:
+            size(int): the size of one side of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get or set the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
+
