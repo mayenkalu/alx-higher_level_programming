6694e7118bf3478803d39f45aa6c15e7e8c53646
Mayen Kalu <kalumayen@gmail.com>
2022-12-20 20:09:52 -0800

    class square with size instance

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -19,8 +19,10 @@ class Square:
             TypeError: if size is an integer.
             ValueError: if size is grater than or equal to 0.
         """
-        if not isinstance(size, int):
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
             raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
