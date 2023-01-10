e3b5d8e01e1ba6818daf8c9002571c3235fd44bd
Mayen Kalu <kalumayen@gmail.com>
2023-01-09 20:15:26 -0800

    fixed code

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,3 +15,4 @@ class BaseGeometry:
         if type(value) is not int:
             raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,4 +1,4 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 """
 Contains the class BaseGeometry and subclass Rectangle
 """
