bc46ebef178a8a4287db392b10d8b29459ccbcd3
Mayen Kalu <kalumayen@gmail.com>
2023-01-10 18:02:10 -0800

    appends a string at the end of a UTF8-text file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,6 @@ Defines a file-writing function.
 
 
 def write_file(filename="", text="")
-    """returns the number of characters of  a string to a UTF-8 text file."""
-    with open(filename, "w", encoding="utf-8") as f:
+    """returns the number of characters written to a UTF-8 text file."""
+    with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
