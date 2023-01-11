aee637811e428097c997ead4e007d749a753569d
Mayen Kalu <kalumayen@gmail.com>
2023-01-10 17:42:36 -0800

    appends a string at the end of a UTF8-text file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,7 @@
 
 
 def append_write(filename="", text="")
-    """return the characters of text appended to UTF-8 text file"""
+    """return the number of characters of string appended to UTF-8 text file"""
     with open(filename, 'a', encoding='utf-8') as f:
         count = f.write(text)
     return count
