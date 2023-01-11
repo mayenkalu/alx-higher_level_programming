371075b70ef0c75a36e3bb1916b802f8cf89ee35
Mayen Kalu <kalumayen@gmail.com>
2023-01-10 18:48:40 -0800

    checks if the file already exists

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+"""
+Python function that add arguments to a Python list
+"""
 
 
 import sys
+import os
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+save_json = __import__('5-save_to_json_file').save_to_json_file
+load_json = __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+my_list = []
+
+if not os.path.exists("./add_item.json"):
+    save_json(my_list, "add_item.json")
+my_list = load_json("add_item.json")
+
+for items in sys.argv[1:]:
+    my_list.append(items)
+save_json(my_list, "add_item.json")
