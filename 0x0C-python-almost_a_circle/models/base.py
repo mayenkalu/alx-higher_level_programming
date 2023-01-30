3ecaff2b6b3a09e17be596384679aa4e052d751a
Mayen Kalu <kalumayen@gmail.com>
2023-01-30 03:26:01 -0800

    second task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python3
 
+"""Define a model class called Base"""
+
+import json
+import turtle
+import csv
+
 class Base:
+    """This class represents a model.
+
+    It will be the "bases" of other classes in this project.
+
+    Attributes:
+        __nb_objects (int): is the number of instantiated Bases.
+    """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """This method initialize a new base.
+        
+        Args:
+            id (int): is the identity of the new base.
+        """
+
         if id is not None:
             self.id = id
 
