d3de0650ae8d4d3690e020e819a60b112f84c5b6
Mayen Kalu <kalumayen@gmail.com>
2023-01-31 05:52:29 -0800

    Dict to instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -41,13 +41,14 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
+        """Returns the list of the JSON string representation"""
         if json_string is None or json_string == '':
             return []
         return json.loads(json_string)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """ Write the JSON string representation of list_objs to a file """
+        """Write the JSON string representation of list_objs to a file """
         filename = cls.__name__ + ".json"
         list_dicts = []
         if list_objs is not None:
@@ -55,3 +56,10 @@ class Base:
                 list_dicts.append(obj.to_dictionary())
         with open(filename, "w") as f:
             f.write(cls.to_json_string(list_dicts))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set"""
+        dummy_instance = cls()
+        dummy_instance.update(**dictionary)
+        return dummy_instance
