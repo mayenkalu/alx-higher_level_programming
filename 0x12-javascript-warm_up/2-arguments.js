04b0ba41095b7bb25e22fc03e3eb3141333341bb
Mayen Kalu <kalumayen@gmail.com>
2023-04-06 13:36:37 -0700

    spaces not tab

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 const count = process.argv.length - 2;
 
 if (count === 0) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (count === 1) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
