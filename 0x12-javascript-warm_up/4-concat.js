d96e711b58659fdeff707d7630c5b43f3331f56a
Mayen Kalu <kalumayen@gmail.com>
2023-04-06 16:08:24 -0700

    still arguments

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,6 +3,10 @@ const [arg1, arg2] = process.argv.slice(2);
 
 if (arg1 && arg2) {
   console.log(`${arg1} is ${arg2}`);
+} else if (arg1) {
+  console.log(`${arg1} is undefined`);
+} else if (arg2) {
+  console.log(`${arg2} is undefined`);
 } else {
   console.log('undefined is undefined');
 }
