b8368a6e00fd8152ee2965d86710719c8bb73792
Mayen Kalu <kalumayen@gmail.com>
2023-04-06 16:01:09 -0700

    still arguments

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,5 +4,5 @@ const [arg1, arg2] = process.argv.slice(2);
 if (arg1 && arg2) {
   console.log(`${arg1} is ${arg2}`);
 } else {
-  console.log('Two arguments are required');
+  console.log('undefined is undefined');
 }
