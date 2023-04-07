b919fa3c7a04d6c60877c6e84f8c4de6e2e8a902
Mayen Kalu <kalumayen@gmail.com>
2023-04-06 17:11:56 -0700

    lines, languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const lines = [
-	  "C is fun",
-	  "Python is cool",
-	  "JavaScript is amazing"
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
 
 lines.forEach((line) => {
