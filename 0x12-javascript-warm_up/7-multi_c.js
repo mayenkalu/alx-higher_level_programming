bb44db378993920fad778c7ea0caf89a7e014db5
Mayen Kalu <kalumayen@gmail.com>
2023-04-07 15:49:36 -0700

    fix error

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,10 +4,9 @@ const arg = process.argv[2];
 const x = parseInt(arg);
 
 if (isNaN(x)) {
-  console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < x; i++) {
-    console.log("C is fun");
+    console.log('C is fun');
   }
 }
-
