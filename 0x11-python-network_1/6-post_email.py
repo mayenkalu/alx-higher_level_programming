4a1ddf1b7dec131193257ead0547247b756b5247
Mayen Kalu <kalumayen@gmail.com>
2023-04-07 21:19:57 +0100

    more requests

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""takes in a URL and an email address, sends a POST request to the
+passed URL with the email as a parameter, and finally displays the body
+of the response.
+
+Eg: ./6-post_email.py <URL> <email>
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
